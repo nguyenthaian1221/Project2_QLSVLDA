c54bfd358220747634970f96599be412a0b3c69e
annt <nguyenthaian1221@gmail.com>
2023-07-10 16:26:09 +0700

    add My Class UI for both SV and GV

---
+++ Project2_QLSVLDA/Areas/Student/Controllers/SVClassController.cs
@@ -0,0 +1,31 @@
+﻿using Project2_QLSVLDA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2_QLSVLDA.Areas.Student.Controllers
+{
+    public class SVClassController : Controller
+    {
+        // GET: Student/SVClass
+        public ActionResult Index()
+        {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("DangNhap", "HomeAdmin", new { area = "Admin" });
+            }
+            else
+            {
+                QL_PROJECTEntities1 db = new QL_PROJECTEntities1();
+                var session = Session["user"].ToString();
+                //List<LOPMONHOC> lop = db.LOPMONHOCs.Where(m => m.magv == session).ToList();
+                List<SINHVIENMONHOC> lop = db.SINHVIENMONHOCs.Where(m => m.mssv == session).ToList();
+                Console.WriteLine(lop);
+
+                return View(lop);
+            }
+        }
+    }
+}
---
+++ Project2_QLSVLDA/Areas/Teacher/Controllers/GVClassController.cs
@@ -0,0 +1,37 @@
+﻿using Project2_QLSVLDA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2_QLSVLDA.Areas.Teacher.Controllers
+{
+    public class GVClassController : Controller
+    {
+        // GET: Teacher/GVClass
+        public ActionResult Index()
+        {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("DangNhap", "HomeAdmin", new { area = "Admin" });
+            }
+            else
+            {
+                QL_PROJECTEntities1 db = new QL_PROJECTEntities1();
+                var session = Session["user"].ToString();
+                List<LOPMONHOC> lop = db.LOPMONHOCs.Where(m => m.magv == session).ToList();
+                Console.WriteLine(lop);
+
+
+
+
+                return View(lop);
+            }
+        }
+
+
+
+
+    }
+}
