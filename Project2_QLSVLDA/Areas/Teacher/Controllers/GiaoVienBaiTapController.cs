0afbc4160c68c6a889760fcf6e2bbea0e3674469
annt <nguyenthaian1221@gmail.com>
2023-07-17 05:50:00 +0700

    tao xong form giao bai tap o ben giao vien || chua luu vao database duoc

--- Project2_QLSVLDA/Areas/Teacher/Controllers/GiaoVienBaiTapController.cs
+++ Project2_QLSVLDA/Areas/Teacher/Controllers/GiaoVienBaiTapController.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿using Project2_QLSVLDA.Content.Custom.Class;
+using Project2_QLSVLDA.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static System.Collections.Specialized.BitVector32;
+
 
 namespace Project2_QLSVLDA.Areas.Teacher.Controllers
 {
     public class GiaoVienBaiTapController : Controller
     {
         // GET: Teacher/GiaoVienBaiTap
+
         public ActionResult Index()
         {
-            return View();
+
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("DangNhap", "HomeAdmin", new { area = "Admin" });
+            }
+            else
+            {
+
+                return View();
+            }
         }
+
+
+
     }
 }
