fdec0f5938d9bc4ecae1ded675828b905a5f1093
annt <nguyenthaian1221@gmail.com>
2023-07-25 17:23:46 +0700

    Complete all feature!!!

--- Project2_QLSVLDA/Areas/Teacher/Controllers/GiaoVienHomeController.cs
+++ Project2_QLSVLDA/Areas/Teacher/Controllers/GiaoVienHomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project2_QLSVLDA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace Project2_QLSVLDA.Areas.Teacher.Controllers
     public class GiaoVienHomeController : Controller
     {
         // GET: Teacher/GiaoVienHome
+        QL_PROJECTEntities1 db = new QL_PROJECTEntities1();
         public ActionResult Index()
         {
             if (Session["user"] == null)
@@ -20,6 +22,43 @@ namespace Project2_QLSVLDA.Areas.Teacher.Controllers
                 return View();
             }
 
+        }
+
+        public JsonResult GetEvents()
+        {
+
+            string str = Session["user"].ToString();
+            var cuochen_allsystem = (from ch in db.CUOCHENs
+                                     join gv in db.GIANGVIENs on ch.magiaovien equals gv.magv
+                                     join svch in db.SINHVIENCUOCHENs on ch.macuochen equals svch.macuochen
+                                     join sv in db.SINHVIENs on svch.masinhvien equals sv.massv
+                                     select new
+                                     {
+                                         sv.ten,
+                                         ch.macuochen,
+                                         ch.magiaovien,
+                                         ch.thoigianbatdau,
+                                         ch.thoigianketthuc,
+                                         ch.diadiem,
+                                         svch.thoigiandat,
+                                         ch.ghichu,
+                                         svch.masinhvien,
+                                         svch.manhom,
+                                         svch.malop
+
+                                     }).ToList();
+
+
+            var list_cuoc_hen = cuochen_allsystem.Where(p => p.magiaovien.ToString().Trim() == str).ToList();
+
+
+            return Json(list_cuoc_hen.AsEnumerable().Select(e => new
+            {
+                title = e.diadiem,
+                start = e.thoigianbatdau.Value.ToString("yyyy/MM/dd HH:mm"),
+                end = e.thoigianketthuc.Value.ToString("yyyy/MM/dd HH:mm")
+
+            }).ToList(), JsonRequestBehavior.AllowGet);
         }
     }
 }
